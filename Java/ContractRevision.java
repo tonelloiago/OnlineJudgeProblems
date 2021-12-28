54be9922bcdfde9548858ce0932dc9c192991f2f
Iago Tonello <tonelloiago@gmail.com>
2021-12-28 17:46:29 -0300

    ContractRevision

---
+++ Java/ContractRevision.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String NStr = new String();
+		String DStr = new String();
+		boolean flag = true;
+		
+		
+		while(flag) {
+			
+			NStr = scan.next();
+			DStr = scan.next();
+
+			if(!(NStr.equals("0") && DStr.equals("0"))) {			
+				
+				DStr = DStr.replaceAll(NStr, "");
+				DStr = DStr.replaceFirst("0*", "");
+			
+				System.out.println(DStr.length() > 0 ? DStr : 0);
+			
+			}else {
+				
+				flag = false;
+				
+			}
+
+		}
+		
+
+	}
+
+}
